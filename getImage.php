0c52163dfc8385553ad67d58dec3f6ba337fd121
marcteys <marc.teys@gmail.com>
2022-06-12 15:57:18 +0200

    feat(Final): Getting final image

---
+++ getImage.php
@@ -0,0 +1,31 @@
+<?php 
+
+include("image.php");
+
+
+// add the "Content-type" header
+header('Content-type: image/jpeg'); 
+ 
+// add a "Expires" header with an offset of 10 min
+$offset = 60 * 10; // (seconds * minutes)    
+$expire = "Expires: " . gmdate("D, d M Y H:i:s", time() + $offset) . " GMT";
+header($expire);
+ 
+// add a "Cache-control" header
+header("Cache-Control: max-age=600, must-revalidate");
+ 
+// Set the image format to JPEG and enable compression
+$im->setImageFormat("jpeg");
+$im->setImageCompression(Imagick::COMPRESSION_JPEG);
+ 
+// Set compression level (1 lowest quality, 100 highest quality)
+$im->setImageCompressionQuality(100);
+ 
+// Strip out unneeded meta data
+$im->stripImage();
+ 
+echo $im;
+exit;
+
+
+ ?>
